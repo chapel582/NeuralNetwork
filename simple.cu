62b67a543038a62c7f0aeba7cf7129a99a1aecca
Trevor Apple <trappleco@gmail.com>
2020-04-24 23:39:04 -0700

    can run profiler, added cuda stuff

--- simple.cu
+++ simple.cu
@@ -1,6 +1,12 @@
 /*
 NOTE: build with nvcc. run setup.bat first
 can rename file with -o option
+
+Can profile with 
+	nvprof .\simple.exe
+I had an issue with the profiler when I ran command line not as an admin.
+Had an error that mentioned something about permission and users
+	
 */
 #include <stdlib.h>
 #include <stdio.h>
@@ -45,3 +51,49 @@ int main(void)
 
 	return 0;
 }
+
+// #include <iostream>
+// #include <math.h>
+// #include <cuda_profiler_api.h>
+
+// // Kernel function to add the elements of two arrays
+// __global__
+// void add(int n, float *x, float *y)
+// {
+//   for (int i = 0; i < n; i++)
+//     y[i] = x[i] + y[i];
+// }
+
+// int main(void)
+// {
+//   int N = 1<<20;
+//   float *x, *y;
+
+//   // Allocate Unified Memory – accessible from CPU or GPU
+//   cudaMallocManaged(&x, N*sizeof(float));
+//   cudaMallocManaged(&y, N*sizeof(float));
+
+//   // initialize x and y arrays on the host
+//   for (int i = 0; i < N; i++) {
+//     x[i] = 1.0f;
+//     y[i] = 2.0f;
+//   }
+
+//   // Run kernel on 1M elements on the GPU
+//   add<<<1, 1>>>(N, x, y);
+
+//   // Wait for GPU to finish before accessing on host
+//   cudaDeviceSynchronize();
+
+//   // Check for errors (all values should be 3.0f)
+//   float maxError = 0.0f;
+//   for (int i = 0; i < N; i++)
+//     maxError = fmax(maxError, fabs(y[i]-3.0f));
+//   std::cout << "Max error: " << maxError << std::endl;
+
+//   // Free memory
+//   cudaFree(x);
+//   cudaFree(y);
+  
+//   return 0;
+// }
